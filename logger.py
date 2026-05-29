9da61225c1adc6e544d88184d1d694aa0b9f878a
ImagoDei0212 <rmtrmtrmt0@gmail.com>
2026-05-29 20:20:25 +0900

    로거 로테이션 파일 핸들러 추가

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import os
+from logging.handlers import RotatingFileHandler
 
 def get_logger(name: str = "api_logger") -> logging.Logger:
     logger = logging.getLogger(name)
@@ -9,7 +10,7 @@ def get_logger(name: str = "api_logger") -> logging.Logger:
     formatter = logging.Formatter(
             "%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d - %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S"
-
+    )
     #터미널 출력 핸들러
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
@@ -17,6 +18,13 @@ def get_logger(name: str = "api_logger") -> logging.Logger:
     log_dir = "logs"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir, exist_ok=True)
-
+    file_handler = RotatingFileHandler(
+        filename=os.path.join(log_dir, "server.log"),
+        maxBytes=5*1024*1024, #5MB
+        backupCount=5,
+        encoding="utf-8"
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 	return logger
 app_logger = get_logger()
