ee205be9c28ccb510854d37bebb62c45971a9db0
ImagoDei0212 <rmtrmtrmt0@gmail.com>
2026-05-29 20:27:19 +0900

    미들웨어 내 클라이언트 요청 정보(IP, Method, URL) 로깅 추가

--- middleware.py
+++ middleware.py
@@ -1,7 +1,12 @@
 from fastapi import Request
 from logger import app_logger
+import time
 
-async def log_requests_middleware(request: Request, call_next)
+async def log_requests_middleware(request: Request, call_next):
+    start_time= time.time()
+    client_ip=request.client.host if request.client else "unknown"
+    app_logger.info(f"요청 수신: [{request.method}] {request.url.path} (IP: {client_ip})")
+
     #API 요청을 가로채는 미들웨어 기본 뼈대
     response = await call_next(request)
     return response
