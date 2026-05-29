379c9e1ed452073746735590e8e574a52d736a12
ImagoDei0212 <rmtrmtrmt0@gmail.com>
2026-05-29 20:05:31 +0900

    로거 뼈대 설정

--- logger.py
+++ logger.py
@@ -1,4 +1,12 @@
 import logging
 import sys
 def get_logger(name: str = "api_logger") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    #터미널 출력 핸들러
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(stream_handler)
+
 	return logger
+app_logger = get_logger()
