eb206fc0c819f78b8caa1f5bf00742a8b412ce16
ImagoDei0212 <rmtrmtrmt0@gmail.com>
2026-05-29 21:40:07 +0900

    자잘한 CSS 버그 픽스 & 로깅 이벤트 추가

--- connect.py
+++ connect.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import date, timedelta
+from typing import Optional
 from notice_model import load_notices, SearchRequest
+
 from middleware import log_requests_middleware
 from logger import app_logger
 
@@ -11,6 +13,10 @@ app.middleware("http")(log_requests_middleware)
 @app.on_event("startup")
 async def startup_event():
     app_logger.info("서버가 성공적으로 시작되었습니다. 로그 기록을 시작합니다.")
+@app.on_event("shutdown")
+async def shutdown_event():
+    #서버 종료 시 안전하게 로그 기록
+    app_logger.info("서버가 안전하게 종료되었습니다.")
 
 # 프론트엔드 연동을 위한 CORS 미들웨어 허용
 app.add_middleware(
