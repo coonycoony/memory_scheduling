88b5d42ba3a769385de4525ce52ce7210051839e
ImagoDei0212 <rmtrmtrmt0@gmail.com>
2026-05-29 20:09:38 +0900

    로거 출력 포맷터 추가 및 연결

--- logger.py
+++ logger.py
@@ -4,8 +4,13 @@ def get_logger(name: str = "api_logger") -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
+    formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+
     #터미널 출력 핸들러
     stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
 
 	return logger
