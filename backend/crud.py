081794e2753339babaac8c1567c477e44f01cc66
ImagoDei0212 <rmtrmtrmt0@gmail.com>
2026-05-30 18:32:01 +0900

    crud.py의 return db.query 제거

--- backend/crud.py
+++ backend/crud.py
@@ -14,9 +14,7 @@ def create_notice(db: Session, univ: str, title: str, url: str, cat: str, date: 
     db.refresh(db_notice)
     return db_notice
 def get_notices(db: Session, university: str, skip: int = 0, limit: int = 100):
-    return db.query(models.NoticeModel).filter(
-            models.NoticeModel.university == university
-    ).offset(skip).limit(limit).all()
+    
 def bulk_insert_notices(db: Session, notices: list):
     # 배열로 들어온 공지사항들을 한 번에 넣기 위한 뼈대
     inserted_count = 0
